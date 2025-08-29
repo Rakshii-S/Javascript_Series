f24aae1b5d81153d3b31f27f8353dcaba04b3103
Rakshitha Shettigar <128218700+Rakshii-S@users.noreply.github.com>
2025-08-29 02:10:43 +0000

    datatypes updated

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -4,6 +4,23 @@
 
 console.log(
     3 
-    +
+    + // -> X code readability should be maintained
     6
 )
+
+//documents to remember tc39 and MDN
+
+//primitive datatypes
+//number, bigint, string, boolean(true / false), null(stand alone value) [value is empty], undefined, symbol(uniqueness)
+
+//object
+
+let name = "Rakshitha"
+let age = 22
+let isLoggedIn = false
+let state
+let status = null
+
+console.log(typeof "Rakshi")
+console.log(typeof null)
+console.log(typeof undefined)
